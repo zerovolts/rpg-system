cb288ee1db49af6e7be225856346451c35a28f24
Zach Stone <hexaphixylix@yahoo.com>
2016-12-12 13:39:19 -0500

    clean up directory structure (add 'src')

---
+++ src/index.js
@@ -0,0 +1,34 @@
+const registry = require('./registry/registry')
+const Actor = require('./actor').Actor
+const Shell = require('./shell').Shell
+const Controller = require('./controller').Controller
+const Market = require('./market').Market
+
+function main() {
+  let actors = Array(8).fill(null)
+  actors = actors.map(_ => new Actor())
+  let actor = actors[0]
+  module.exports.actor = actor
+
+  actor.addItem('iron-ore', 4)
+  actor.addItem('leather', 2)
+
+  actor.craftItem('iron-bar', 2)
+  actor.craftItem('iron-dagger')
+
+  let controllers = actors.map(a => new Controller(a))
+  for (let con of controllers) {
+    con.start()
+  }
+
+  let shell = new Shell(actor)
+  shell.start()
+
+  let market = new Market()
+  console.log(actor.inventory)
+  market.sell(actor, 'iron-dagger', 1, 24)
+  console.log(actor.inventory)
+  console.log(market.listings)
+}
+
+module.exports.main = main
